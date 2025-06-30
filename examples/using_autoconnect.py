b89ff2a12d1e1af9abd0ad0312aea9c4d2ec0e86
LC_Linkous <L.C.Linkous@gmail.com>
2025-06-29 20:21:38 -0400

    autoconnect checked

---
+++ examples/using_autoconnect.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+##-------------------------------------------------------------------------------\
+#   tinySA_python
+#   './examples/using_autoconnect.py'
+#   This is an example of using the autoconnect feature. 
+#   The detected device info is returned and the serial disconnected
+#
+#   Last update: June 18, 2025
+##-------------------------------------------------------------------------------\
+
+# import nanoVNA library
+# (NOTE: check library path relative to script path)
+from src.nanoVNA_python import nanoVNA 
+
+# create a new nanoVNA object    
+nvna = nanoVNA()
+
+# set the return message preferences 
+nvna.set_verbose(True) #detailed messages
+nvna.set_error_byte_return(True) #get explicit b'ERROR' if error thrown
+
+
+# attempt to autoconnect
+found_bool, connected_bool = nvna.autoconnect()
+
+# if port found and connected, then complete task(s) and disconnect
+if connected_bool == True: 
+    print("device connected")
+
+    msg = nvna.get_info() 
+    print(msg)
+    
+
+    nvna.disconnect()
+else:
+    print("ERROR: could not connect to port")
